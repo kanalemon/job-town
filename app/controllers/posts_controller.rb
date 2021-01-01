a0a2e821253e9f0cd7f9d64cc69fb1148f3f4071
kanalemon <kanalemon@icloud.com>
2021-01-01 20:51:48 +0900

    投稿画面のビュー作成、保存処理

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,23 @@
 class PostsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:campany, :department, :industry, :occupation, :description, :skill)
+  end
+
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_21_014237) do
+ActiveRecord::Schema.define(version: 2020_12_28_044442) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "campany"
+    t.string "department"
+    t.string "industry"
+    t.string "occupation"
+    t.string "description"
+    t.string "skill"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -28,4 +41,5 @@ ActiveRecord::Schema.define(version: 2020_12_21_014237) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
