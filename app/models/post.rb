f740816e9dc500efecd55b41b75e8620d62352d3
kanalemon <kanalemon@icloud.com>
2021-01-02 13:12:02 +0900

    バリデーション

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+
+  belongs_to :user
+  has_many :comments
+
+  with_options presence: true do
+    validates :campany
+    validates :department
+    validates :industry
+    validates :occupation
+    validates :description
+    validates :skill
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :posts
+  has_many :comments
+
+  with_options presence: true do
+    validates :nickname
+    validates :age
+    validates :gender
+    validates :career
+
 end
